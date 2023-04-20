f45951fbb11833c12534fe86c3442711e52bfd81
cgiaccone-tt <christopher.giaccone@tributetech.com>
2023-04-20 13:14:26 -0400

    Added basic validation and a button to reset the application

--- index.php
+++ index.php
@@ -13,10 +13,16 @@ include_once 'classes/CensorText.php';
 //check if form has been submitted, if not, display the empty form,
 //if so, display the form with the censored document results
 if (isset($_POST) && !empty($_POST)) {
-    $wordList = CensorText::createWordListArray($_POST['censoredWordList']);
+    $wordArr = CensorText::createWordListArray($_POST['censoredWordList']);
+    //remove empty elements from array
+    $wordArr = array_filter($wordArr, function($val){return !empty($val);});
+    //remove whitespace from array elements
+    $wordArr = array_map('trim', $wordArr);
+    //create a string of words with quotes around them to display in the input field
+    $wordList = implode(', ', array_map(function($val){return sprintf("'%s'", $val);}, $wordArr));
     //use htmlspecialchars to prevent XSS.  The output is in a text area so it is probably not necessary but it
     //is a good practice to use it.
-    $docCensored = htmlspecialchars(CensorText::censorDocumentText($wordList, $_POST['docToCensor']));
+    $docCensored = htmlspecialchars(CensorText::censorDocumentText($wordArr, $_POST['docToCensor']));
 
     //output form with censored document and input text
     $censoredHtml = <<<EOT
@@ -27,6 +33,15 @@ if (isset($_POST) && !empty($_POST)) {
                 <title>DOD</title>
                 <link rel="stylesheet" href="css/main.css">
                 <script type="text/javascript">
+                    function validateForm(event) {
+                        let censoredWordList = document.forms["censorApp"]["censoredWordList"].value;
+                        let docToCensor = document.forms["censorApp"]["docToCensor"].value;
+                        if (censoredWordList == "" || docToCensor == "") {
+                            alert("Please enter a list of words to censor and a document to censor.");
+                            return false;
+                        }
+						document.censorApp.submit();
+                    }
                     //copy the censored document to the clipboard
                     function copyToClipboard() {
                         let copyText = document.getElementById("docCensored");
@@ -36,6 +51,11 @@ if (isset($_POST) && !empty($_POST)) {
                         document.execCommand("copy");
 						copyText.disabled = true;
                     }
+					
+					function resetApplication(){
+						//redirect to index.php
+                        window.location.href = "index.php";
+					}
                 </script>
             </head>
             <body>
@@ -44,13 +64,13 @@ if (isset($_POST) && !empty($_POST)) {
                     <h3>Document Censor Application Demonstration</h3>
                     <p>Enter a list of words to censor encapsulated by single or double quotes and separated by commas or spaces. 
                     Then, copy and past the document to censor in the filed provided.</p>
-                    <form id="censorApp" action="index.php" method="post">
+                    <form name="censorApp" id="censorApp" action="index.php" method="post" onsubmit="event.preventDefault(); validateForm()">
                         <div class="docCensoredContainer">
                             <div>
                                 <label for="censoredWordList" class="labelClass">Keywords</label>
                             </div>
                             <div>
-                                <input type="text" name="censoredWordList" class="censoredWordList" value="{$_POST['censoredWordList']}">
+                                <input type="text" name="censoredWordList" class="censoredWordList" value="{$wordList}">
                             </div>
                             <div>
                                 <label for="docToCensor" class="labelClass">Document</label>
@@ -67,6 +87,7 @@ if (isset($_POST) && !empty($_POST)) {
                             <div>
                                 <p>
                                     <button type="button" class="copyBtn" onclick="copyToClipboard()">Copy to clipboard</button>
+                                    <button type="button" class="clearBtn" onclick="resetApplication()">Reset Application</button>
                                 </p>
                                 <textarea name="docCensored" class="docCensored" id="docCensored" disabled>{$docCensored}</textarea>
                             </div>
@@ -86,6 +107,18 @@ EOT;
                     <meta charset="UTF-8">
                     <title>DOD</title>
                     <link rel="stylesheet" href="css/main.css">
+                    <script type="text/javascript">
+                    //basic form validation
+                    function validateForm(event) {
+                        let censoredWordList = document.forms["censorApp"]["censoredWordList"].value;
+                        let docToCensor = document.forms["censorApp"]["docToCensor"].value;
+                        if (censoredWordList == "" || docToCensor == "") {
+                            alert("Please enter a list of words to censor and a document to censor.");
+                            return false;
+                        }
+						document.censorApp.submit();
+                    }
+                    </script>
                 </head>
                 <body>
                     <h2>Department of Defense</h2>
@@ -93,7 +126,7 @@ EOT;
                         <h3>Document Censor Application Demonstration</h3>
                         <p>Enter a list of words to censor encapsulated by single or double quotes and separated by commas or spaces. 
                         Then, copy and past the document to censor in the filed provided.</p>
-                        <form id="censorApp" action="index.php" method="post">
+                        <form name="censorApp" id="censorApp" action="index.php" method="post" onsubmit="event.preventDefault(); validateForm()">
                             <div class="docCensoredContainer">
                                 <div>
                                     <label for="censoredWordList" class="labelClass">Keywords</label>
